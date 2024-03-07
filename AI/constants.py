2883346238a798b45cc926a4960f07b86a0b3642
hqvjet <viethq.1906@gmail.com>
2024-03-07 09:20:45 +0700

    first step model

---
+++ AI/constants.py
@@ -0,0 +1,23 @@
+# Directions
+PATH = 'res/'
+IMAGE_DATASET = 'violence_dataset/'
+VECTORIZED_DATASET = 'vectorized_dataset/'
+SUMMARY = 'model_summary/'
+MODEL = 'model/'
+CHART = 'chart/'
+REPORT = 'report/'
+
+TSN_SUMMARY = 'TSN_SUM.png'
+TSN_MODEL = 'TSN.h5'
+TSN_CHART = 'TSN_CHART.png'
+TSN_REPORT = 'TSN_REPORT.txt'
+
+# Var
+MAX_FRAME = 8
+STOP_PATIENCE = 10
+EPOCH = 200
+LR = 0.0001
+BATCH_SIZE = 8
+SHAPE = (224, 224, 3)
+FINAL_LAYER_NUMBER = 2
+DEFAULT_IMAGE_DIMENSION = (500, 500)
