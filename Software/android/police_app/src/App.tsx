28853516e6e3977ef8b22a29772cedb9b3e60a68
hqvjet <viethq.1906@gmail.com>
2024-07-25 15:18:17 +0700

    fixed

---
+++ Software/android/police_app/src/App.tsx
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type { PropsWithChildren } from 'react';
+import {
+  ScrollView,
+  Text,
+  Image,
+  TextInput,
+  View,
+} from 'react-native';
+import tw from 'twrnc';
+
+import Layout from './layout/Layout';
+
+function App(): React.JSX.Element {
+
+  return (
+    <Layout>
+      
+    </Layout>
+  );
+}
+
+export default App;
---
+++ Software/android/police_app/src/Authen/Login.tsx
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type { PropsWithChildren } from 'react';
+import {
+  ScrollView,
+  Text,
+  Image,
+  TextInput,
+  View,
+} from 'react-native';
+import tw from 'twrnc';
+
+function Login(): React.JSX.Element {
+
+  return (
+    <ScrollView>
+      <Text>Some text</Text>
+      <View>
+        <Text>Some more text</Text>
+        <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{width: 200, height: 200}}
+        />
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        defaultValue="You can type in me"
+      />
+    </ScrollView>
+  );
+}
+
+export default Login;
